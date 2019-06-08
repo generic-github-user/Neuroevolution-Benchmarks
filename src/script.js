6a2fcce8382bc650ec864823b2915b27ee84a650
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-08 14:43:19 -0400

    Add TensorFlow.js network

--- src/script.js
+++ src/script.js
@@ -1,2 +1,6 @@
 var n = neataptic
 var neat_network = new neataptic.Network(1, 1);
+
+const tf_network = tf.sequential();
+tf_network.add(tf.layers.dense({units: 5, inputShape: [1], activation: 'relu'}));
+tf_network.add(tf.layers.dense({units: 1}));
