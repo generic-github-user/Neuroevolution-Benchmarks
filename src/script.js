563638ade7b5c068bd3b71ef978af4b002cc1f94
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-08 17:16:15 -0400

    Add generation of training data for Neataptic

--- src/script.js
+++ src/script.js
@@ -44,14 +44,27 @@ const graph = new Chart(ctx, {
 const inputs = tf.randomUniform([10]);
 const outputs = inputs.square();
 
+var a = inputs.dataSync();
+b = [];
+// replace this with map?
+a.forEach(
+	(c) => {
+		b.push({
+			input: [c],
+			output: [c ** 2]
+		})
+	}
+)
+
 var n = neataptic
 var neat_network = new neataptic.Network(1, 1);
 
 const tf_network = tf.sequential();
 tf_network.add(tf.layers.dense({units: 5, inputShape: [1], activation: 'relu'}));
 tf_network.add(tf.layers.dense({units: 1}));
-
 tf_network.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+
+
 (async () => {
 	for (let i = 0; i < 10; ++i) {
 	  const h = await tf_network.fit(inputs, outputs, {
