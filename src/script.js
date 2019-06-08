afa7df20e3544841bd1e17c42a78053f47fcbb5a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-08 14:52:31 -0400

    Train TFJS model using model.fit()
    https://js.tensorflow.org/api/latest/#tf.LayersModel.fit

--- src/script.js
+++ src/script.js
@@ -9,3 +9,12 @@ tf_network.add(tf.layers.dense({units: 5, inputShape: [1], activation: 'relu'}))
 tf_network.add(tf.layers.dense({units: 1}));
 
 tf_network.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+(async () => {
+	for (let i = 0; i < 10; ++i) {
+	  const h = await tf_network.fit(inputs, outputs, {
+		  batchSize: 5,
+		  epochs: 3
+	  });
+	  console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
+	}
+})();
