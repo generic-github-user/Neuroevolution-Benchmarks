b41da26c905f6872df7a10beb38e6d36f66ac706
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-08 15:44:42 -0400

    Add data for chart

--- src/script.js
+++ src/script.js
@@ -12,12 +12,17 @@ tf_network.add(tf.layers.dense({units: 5, inputShape: [1], activation: 'relu'}))
 tf_network.add(tf.layers.dense({units: 1}));
 
 tf_network.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+var labels = [];
+var tf_losses = [];
 (async () => {
 	for (let i = 0; i < 10; ++i) {
 	  const h = await tf_network.fit(inputs, outputs, {
 		  batchSize: 5,
-		  epochs: 3
+		  epochs: 1
 	  });
 	  console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
+	  labels.push(i);
+	  tf_losses.push(h.history.loss[0]);
+	  console.log(h.history.loss)
 	}
 })();
