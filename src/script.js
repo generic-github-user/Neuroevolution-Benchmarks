8818385ce47984d4342fc24b24d666d02f5c0f97
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2019-06-08 14:50:24 -0400

    Compile model

--- src/script.js
+++ src/script.js
@@ -7,3 +7,5 @@ var neat_network = new neataptic.Network(1, 1);
 const tf_network = tf.sequential();
 tf_network.add(tf.layers.dense({units: 5, inputShape: [1], activation: 'relu'}));
 tf_network.add(tf.layers.dense({units: 1}));
+
+tf_network.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
